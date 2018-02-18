6f4fa87876cf7941ef1ea7aa6df02b0a7ffe926a
ArtiomKolesnikov <77777green77777@gmail.com>
2018-02-18 23:33:42 +0300

    картинки

--- resources/views/index.php
+++ resources/views/index.php
@@ -8,12 +8,60 @@
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 
     <link href="css/app.css" rel="stylesheet" type="text/css">
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <title>Avto Rescue</title>
 </head>
 <body>
-<div class="block">
-    <h1 class="title_">Avto Rescue</h1>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <span class="">Частные эвакуаторы Москвы и Подмосковья, спешат на помощь!</span>
+        </div>
+        <div class="col mr-auto text-right">
+            <span>Самые низкие, договорные цены <span>от 700 рублей</span></span>
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col">
+
+            <img src="/images/evac_mult.jpg" class="img-thumbnail" alt="evac_mult">
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <span>Все виды эвакуаторов:</span>
+            <ul>
+                <li>С ломанной платформой</li>
+                <li>Со сдвижной платформой</li>
+                <li>Манипулятор</li>
+                <li>С частичной погрузкой</li>
+            </ul>
+        </div>
+    </div
+    >
+    <div class="row">
+        <div class="col">
+            <span>Ежедневно на линии более 50-ти эвакуаторов</span>
+            <span>Мы всегда ближе, дешевле, быстрее</span>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col text-center">
+
+            <button class="btn btn-default">
+                <img src="/images/btn_call.jpg" width="200" />
+            </button>
+        </div>
+    </div>
 </div>
+
+
+
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 </body>
 </html>
