169ef49a459386e53bcee9573cc0245396509209
ArtiomKolesnikov <77777green77777@gmail.com>
2018-02-18 21:55:29 +0300

    install webpack/mixer

--- resources/views/index.php
+++ resources/views/index.php
@@ -5,9 +5,15 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+    <link href="css/app.css" rel="stylesheet" type="text/css">
+
     <title>Avto Rescue</title>
 </head>
 <body>
-<h1>Avto Rescue</h1>
+<div class="block">
+    <h1 class="title_">Avto Rescue</h1>
+</div>
 </body>
 </html>
